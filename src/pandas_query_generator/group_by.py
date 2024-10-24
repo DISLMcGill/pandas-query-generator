59432d1547ad2dce7f85a23a0c95f9f4ea34ad91
Liam <liam@scalzulli.com>
2024-10-23 23:35:03 -0400

    Format

--- src/pandas_query_generator/group_by.py
+++ src/pandas_query_generator/group_by.py
@@ -12,5 +12,5 @@ class GroupByAggregation(Operation):
   def apply(self, entity: str) -> str:
     group_cols = ', '.join(f"'{col}'" for col in self.group_by_columns)
     numeric_only = 'numeric_only=True' if self.agg_function != 'count' else ''
-    formatted_option = f", {numeric_only}" if numeric_only else ""
+    formatted_option = f', {numeric_only}' if numeric_only else ''
     return f".groupby(by=[{group_cols}]).agg('{self.agg_function}'{formatted_option})"
