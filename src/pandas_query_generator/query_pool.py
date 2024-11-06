5705994ce3abfebc35ce8e749203e6c3d1ca38f8
Liam <liam@scalzulli.com>
2024-11-06 13:36:16 -0500

    Add more

--- src/pandas_query_generator/query_pool.py
+++ src/pandas_query_generator/query_pool.py
@@ -1,5 +1,26 @@
+import multiprocessing
+import typing as t
 from dataclasses import dataclass
+
+import pandas as pd
+
+from .query import Query
+from .utils import execute_query
+
+
+def execute_query_wrapper(args):
+  query, sample_data = args
+  return execute_query(query, sample_data)
+
 
 @dataclass
 class QueryPool:
-  ...
+  queries: t.List[Query]
+
+  def execute(
+    self, sample_data: t.Dict[str, pd.DataFrame]
+  ) -> t.List[t.Tuple[t.Optional[t.Union[pd.DataFrame, pd.Series]], t.Optional[str]]]:
+    with multiprocessing.Pool() as pool:
+      return list(
+        pool.imap(execute_query_wrapper, ((query, sample_data) for query in self.queries)),
+      )
