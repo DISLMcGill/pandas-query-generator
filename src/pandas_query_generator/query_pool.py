c2db787872204f23a5c30afce32f43a679fa49cc
Liam <liam@scalzulli.com>
2024-11-06 13:40:56 -0500

    Simplify

--- src/pandas_query_generator/query_pool.py
+++ src/pandas_query_generator/query_pool.py
@@ -1,8 +1,10 @@
 import multiprocessing
 import typing as t
 from dataclasses import dataclass
+from functools import partial
 
 import pandas as pd
+from tqdm import tqdm
 
 from .query import Query
 from .utils import execute_query
@@ -18,9 +20,19 @@ class QueryPool:
   queries: t.List[Query]
 
   def execute(
-    self, sample_data: t.Dict[str, pd.DataFrame]
+    self, sample_data: t.Dict[str, pd.DataFrame], with_status: bool = False
   ) -> t.List[t.Tuple[t.Optional[t.Union[pd.DataFrame, pd.Series]], t.Optional[str]]]:
+    f = partial(execute_query, sample_data=sample_data)
+
     with multiprocessing.Pool() as pool:
+      if not with_status:
+        return list(pool.imap(f, self.queries))
+
       return list(
-        pool.imap(execute_query_wrapper, ((query, sample_data) for query in self.queries)),
+        tqdm(
+          pool.imap(f, self.queries),
+          total=len(self.queries),
+          desc='Executing queries',
+          unit='query',
+        )
       )
