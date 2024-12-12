437181a4338aa0c7963f0a3e5b66a11d69169a0f
Liam <liam@scalzulli.com>
2024-12-11 20:02:18 -0500

    Format + link

--- www/src/components/navbar.tsx
+++ www/src/components/navbar.tsx
@@ -1,12 +1,14 @@
 import { Button } from '@/components/ui/button';
-import { Github } from 'lucide-react';
+import { Book, Github } from 'lucide-react';
 
 import { ModeToggle } from './mode-toggle';
 
 export const Navbar = () => (
   <div className='flex items-center justify-between bg-card p-4'>
     <div>
-      <p className='text-3xl font-semibold'>Pandas Query Generator 🐼</p>
+      <a href='/'>
+        <p className='text-3xl font-semibold'>Pandas Query Generator 🐼</p>
+      </a>
       <p className='text-md text-muted-foreground'>
         A web interface for the{' '}
         <a href='https://pypi.org/project/pqg/' target='blank'>
@@ -16,6 +18,14 @@ export const Navbar = () => (
       </p>
     </div>
     <div className='flex items-center space-x-4'>
+      <Button variant='outline' size='icon' asChild>
+        <a
+          href='https://dislmcgill.github.io/pandas-query-generator/docs/index.html'
+          target='_blank'
+        >
+          <Book className='h-4 w-4' />
+        </a>
+      </Button>
       <Button variant='outline' size='icon' asChild>
         <a
           href='https://github.com/DISLMcGill/pandas-query-generator'
--- www/vite.config.ts
+++ www/vite.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/pandas-query-generator",
+  base: '/pandas-query-generator',
   plugins: [react()],
   resolve: {
     alias: {
